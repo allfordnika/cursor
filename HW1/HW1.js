b268dc390eb800dccbcfcc8855995e53ca8891c6
Nika <allfrod.nika@gmail.com>
2020-01-30 22:05:10 +0200

    changes in code line 47

--- HW1/HW1.js
+++ HW1/HW1.js
@@ -8,18 +8,14 @@ console.log(Math.max(carrot, avocado, tomato));
 Math.min(carrot,avocado,tomato);
 console.log(Math.min(carrot,avocado,tomato));
 
-carrot+avocado+tomato
-console.log(carrot+avocado+tomato);
-
-let vegetables = 229.8775;
+let vegetables = (carrot+avocado+tomato);
 console.log(vegetables);
 
 Math.trunc(vegetables);
 console.log(Math.trunc(vegetables));
 
-let x = Math.trunc(carrot + avocado + tomato);
-x + (100 - (x%100))
-console.log(x + (100 - (x%100)));
+console.log((vegetables/100).toFixed(0) * 100)
+
 
 Boolean(229 % 2)
 console.log(Boolean(229 % 2))
@@ -49,7 +45,7 @@ console.log(diff);
 console.log("Максимальная цена:" + Math.max(carrot, avocado, tomato));
 console.log("Минимальная цена:" + Math.min(carrot, avocado, tomato));
 console.log("Целая часть стоимости:" + Math.trunc(vegetables));
-console.log("Сумма товаров округленная до сотен:" + (x + (100 - (x%100))));
+console.log("Сумма товаров округленная до сотен:" + ((vegetables/100).toFixed(0) * 100));
 console.log("Булевое значение:" + Boolean(229 % 2));
 console.log("Сумма сдачи:" + change);
 console.log("Среднее значение:" + averagePrice.toFixed(2));
